f9b6c4c31de5c6ecc7e4d532c36698412a319491
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-04-12 09:06:16 -0700

    base code

---
+++ ExtDomainModel/ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by studentuser on 4/12/17.
+//  Copyright © 2017 Ethan.McGregor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
